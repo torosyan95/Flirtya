cab23c4f6b71097781369c5431a1d8ce50c3954b
Karen <84942855+torosyan95@users.noreply.github.com>
2025-04-28 04:01:01 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
+// Настройка подключения к вашему проекту Firebase
+
 const firebaseConfig = {
   apiKey: "AIzaSyBuT_w-LYazd9NGgqElTt6X6286vWz7L78",
   authDomain: "flirtya.firebaseapp.com",
+  databaseURL: "https://flirtya-default-rtdb.firebaseio.com",
   projectId: "flirtya",
-  storageBucket: "flirtya.firebasestorage.app",
+  storageBucket: "flirtya.appspot.com",
   messagingSenderId: "546757161051",
   appId: "1:546757161051:web:d6a4108eb3ba931113cf7e",
   measurementId: "G-LWJ3ZSXGJ8"
 };
 
+// Инициализация Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-const firebaseAuth = firebase.auth();
-const firebaseStorage = firebase.storage();
-
-const firebaseCreate = (auth, email, password) => auth.createUserWithEmailAndPassword(email, password);
-const firebaseSignOut = (auth) => auth.signOut();
